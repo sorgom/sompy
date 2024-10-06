3e54beeb2a630642430ee577a81b2979e9e5db53
sorgom <110917257+sorgom@users.noreply.github.com>
2024-10-07 00:38:51 +0200

    progress

--- lineNumberedFind.py
+++ lineNumberedFind.py
@@ -1,59 +1,69 @@
 import re
 from collections import defaultdict
-from pprint import pprint
 
 class LineNumberedFind(object):
-    """Finds patterns in files and returns content found with line numbers."""
+    """Finds patterns in files and returns content found with line numbers"""
     
-    def __init__(self, inclBlocks:bool=False):
-        """Initialize finder. Optionally let include text blocks found."""
+    def __init__(self):
+        """initialize finder"""
         self.lookups = dict()
         self.results = defaultdict(list)
-        self.inclBlocks = inclBlocks
 
     def add(self, key, pattern, flags=re.NOFLAG):
-        """Add a pattern to look for in the files."""
+        """add a pattern to look for in the files"""
+        # this is the 1st clou of the search:
+        # - 1st  match: the whole pattern wrapped in a group
+        # - last match: alternatively a newline
         self.lookups[key] = re.compile(r'(' + pattern + r')|(\n)', flags)
         return self
 
     def sFile(self, fp:str):
-        """Search for the patterns in a file."""
+        """search for the patterns in a file"""
         with open(fp, 'r') as fh:
             cont = fh.read()
             fh.close()
             for key, rx in self.lookups.items():
                 lnr = 1
-                for snip, *res, br in rx.findall(cont):
-                    if snip:
-                        self.results[key].append([fp, lnr, snip, *res] if self.inclBlocks else [fp, lnr, *res])
-                        lnr += snip.count('\n')
-                    elif br:
-                        lnr += 1
+                for all, *res, br in rx.findall(cont):
+                    # this is the 2nd clou of the search:
+                    # pattern or newline matched
+                    # - if pattern matched: 
+                    #   - store content found with file and line number
+                    #   - add the content number of newlines to the line numbers' counter
+                    if all:
+                        self.results[key].append([fp, lnr, all, *res])
+                        lnr += all.count('\n')
+                    # - otherwise just increase numbers' counter
+                    else: lnr += 1
 
     def sFiles(self, fps:list[str]):
-        """Search for the patterns in a list of files."""
+        """search for the patterns in a list of files"""
         for fp in fps:
             self.sFile(fp)
 
 if __name__ == '__main__':
     from os.path import dirname, join
     from glob import glob
-    lnf = LineNumberedFind(inclBlocks=False)
-    lnf.add('classes and methods', r'^(?:class +(\w+)|    def +(\w+))([^\n]*)(?:\n +"""(.*?)""")?', re.M | re.S)
-    lnf.sFiles(glob(join(dirname(__file__), '*.py'), recursive=True))
+    lnf = LineNumberedFind()
+    # simple class or method pattern
+    # $1: class name
+    # $2: method name
+    # $3: signature
+    # $4: docstring
+    lnf.add('CLASSES AND METHODS', r'^(?:class +(\w+)|    def +(\w+))([^\n]*)(?:\n +"""(.*?)""")?', re.M | re.S)
+    
+    # search for classes and methods in all .py files this directory and below
+    mydir = dirname(__file__)
+    files = glob(join(mydir, '*.py')) + glob(join(mydir, '*', '*.py'), recursive=True)
+    lnf.sFiles(files)
 
     for key, res in lnf.results.items():
-        print('#### ', key)
-        fnp = None
-        for fp, lnr, cla, met, sig, doc in res:
-            if fnp != fp:
-                print('##', fp)
-                fnp = fp
-            print(f'[{lnr:4d}]', doc)
-            if cla: print(cla, end='')
-            else: print(' ' * 6, met, end='')
-            print(sig)
-    
-# rx = re.compile(r'^\d+') #, re.M)
-
-# print("Regex:", rx, type(rx), rx.pattern, rx.flags, type(re.M), re.NOFLAG.value)
+        print('=' * 6, key)
+        currfp = None
+        for fp, lnr, all, cla, met, sig, doc in res:
+            if currfp != fp:
+                print('FILE: ', fp)
+                currfp = fp
+            print(f'[{lnr:4d}] {doc}')
+            if cla: print('CLASS:', cla)
+            else: print(' ' * 6, f'{met}{re.sub(r':$', '', sig)}')
