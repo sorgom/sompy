72b4cdcf21eaae618fdcac5ce029697681cb36a4
sorgom <110917257+sorgom@users.noreply.github.com>
2024-10-27 23:48:46 +0100

    bullseye HTML with index

--- bullseye/covbr.py
+++ bullseye/covbr.py
@@ -15,42 +15,51 @@ from os.path import isfile, isdir
 import sompy
 from docOpts import docOpts
 
-rFile = r'\w+(?:/\w+)*\.(?:cpp|h):'
-rEclip = r'(?: +\.\.\.\n)?'
-rxDouble = re.compile(rf'^{rEclip}(?:{rFile}\n)*({rFile})', re.M)
-rxTail = re.compile(rf'{rEclip}(?:{rFile})?\s*$')
-rxSpc = re.compile(rf'\s+(\n{rFile})')
+class Covbr(object):
+    def __init__(self, doc=__doc__) -> None:
+        self.rFile = r'\w+(?:/\w+)*\.(?:cpp|h):'
+        self.rEclip = r'(?: +\.\.\.\n)?'
+        self.rxDouble = re.compile(rf'^{self.rEclip}(?:{self.rFile}\n)*({self.rFile})', re.M)
+        self.rxTail = re.compile(rf'{self.rEclip}(?:{self.rFile})?\s*$')
+        self.rxSpc = re.compile(rf'\s+(\n{self.rFile})')
+        self.doc = doc
+        self.postFunc = None
+        self.opts = {}
 
-def reduceCovbr(fp:str, func=None):
-    with open(fp, 'r') as fh:
-        cont = fh.read()
-        fh.close()
-        newc = re.sub(r'\s+$', '', rxTail.sub('', rxSpc.sub(r'\n\1', rxDouble.sub(r'\n\1', cont.replace('\r', ''))))) + '\n'
-        if func:
-            func(fp, newc)
-        elif newc != cont:
-            print('->', fp)
-            with open(fp, 'w') as fh:
-                fh.write(newc)
-                fh.close()
+    def process(self, fp:str):
+        with open(fp, 'r') as fh:
+            cont = fh.read()
+            fh.close()
+            newc = re.sub(r'\s+$', '', 
+                self.rxTail.sub('', 
+                    self.rxSpc.sub(r'\n\1', 
+                        self.rxDouble.sub(r'\n\1', cont.replace('\r', ''))))) + '\n'
+            if self.postFunc:
+                self.postFunc(fp, newc)
+            elif newc != cont:
+                print('->', fp)
+                with open(fp, 'w') as fh:
+                    fh.write(newc)
+                    fh.close()
 
-def reduceCovbrDir(dir:str, pattern:str, func=None):
-    cdir = getcwd()
-    chdir(dir)
-    for fp in glob(pattern):
-        reduceCovbr(fp, func)
-    chdir(cdir)
+    def processDir(self, dir:str, pattern:str):
+        cdir = getcwd()
+        chdir(dir)
+        for fp in glob(pattern):
+            self.process(fp)
+        chdir(cdir)
 
-def reduceCovbrCLI(func=None, doc=__doc__):
-    opts, args = docOpts(doc, reqArgs=True)
-    for arg in args:
-        if isfile(arg):
-            reduceCovbr(arg, func=func)
-        elif isdir(arg):
-            reduceCovbrDir(arg, opts.get('g', 'todo_*.txt'), func=func)
-        else:
-            print(f'"{arg}" is neither a file nor a directory')
+    def processCLI(self):
+        opts, args = docOpts(self.doc, reqArgs=True)
+        self.opts = opts
+        for arg in args:
+            if isfile(arg):
+                self.process(arg)
+            elif isdir(arg):
+                self.processDir(arg, opts.get('g', 'todo_*.txt'))
+            else:
+                print(f'"{arg}" is neither a file nor a directory')
 
 if __name__ == '__main__':
-    reduceCovbrCLI()
+    Covbr().processCLI()
 
--- bullseye/covbr_html.py
+++ bullseye/covbr_html.py
@@ -5,17 +5,33 @@ Usage: this script [options] folders / files
 options:
     -g  <glob> glob pattern of the report files
         default: "todo_*.txt"
+    -i  <num> create index of sources at least <num> sources listed
     -h  help
 """
-from covbr import reduceCovbrCLI, rFile
+from covbr import Covbr
 import re
 from itertools import batched
+from collections import Counter
 from os.path import basename, isfile
+from html import escape 
 
-rxSplit = re.compile(rf'^\n*({rFile})\n*', re.M)
+class CovbrHtml(Covbr):
+    def __init__(self, doc=__doc__) -> None:
+        super().__init__(doc)
+        self.postFunc = self.process
+        self.rxSplit = re.compile(rf'^\n*({self.rFile})\n*', re.M)
+        self.rx_ok = re.compile(r'^( *)(X|TF|tf)( .*)?', re.M)
+        self.rx_nok = re.compile(r'^( *)--&gt;(\w+)?( .*)?', re.M)
+        self.rx_id = re.compile(r'\W')
+        self.postFunc = self._2html
+        self.counter = Counter()
+        self.curr = None
+        self.map = {}
 
-def getHtmlHeader(title:str):
-    return """<!DOCTYPE html>
+    @staticmethod
+    def _header(title:str):
+        return """
+<!DOCTYPE html>
 <html lang=en>
 <head>
 <title>##TITLE</title>
@@ -24,8 +40,10 @@ def getHtmlHeader(title:str):
 * { font-style: normal; text-decoration: none;
 	font-family:Consolas,Consolas,Menlo,monospace;
 	margin-top: 0;margin-bottom: 0;
+    white-space:pre;
 }
-p { font-size:10pt; white-space:pre }
+a { display: block; margin: 0; padding: 0; }
+p { font-size:10pt; }
 h3 { padding-left:1em; background-color: rgb(227, 227, 227); font-weight: normal; padding-bottom: 2px;}
 span { background-color: hsl(120,100%,93%); }
 span > u { color: blue; font-weight: bold; }
@@ -35,64 +53,89 @@ span > s { color: red; font-weight: bold; }
 </head>
 <body>
 """.replace('##TITLE', title)
+    
+    @staticmethod
+    def _footer():
+        return '</body></html>'
 
-def getHtmlFooter():
-    return '</body></html>'
+    @staticmethod
+    def _padd(otag:str, n:int, tag:str, ol:int=0):
+        return tag + ' ' * (len(otag) + ol - n)
 
-def _padd(otag:str, n:int, tag:str):
-    return tag + ' ' * (len(otag) - n)
+    def _replNok(self, mo):
+        """indication: not covered"""    
+        self.counter[self.curr] += 1
+        ind, what, line = mo.groups('')
+        n = 2
+        match what:
+            case 'T': tag = f'<u>T</u><s>F</s>'
+            case 'F': tag = f'<s>T</s><u>F</u>'
+            case 't': tag = f'<u>t</u><s>f</s>'
+            case 'f': tag = f'<s>t</s><u>f</u>'
+            case _:
+                tag = f'<s>X</s>'
+                n = 1
+        return f'<span class=x>{ind}{self._padd(what, n, tag, 3)}{line}</span>'
 
-def _replNok(mo):
-    """indication: not covered"""    
-    ind, otag, what, line = mo.groups('')
-    n = 2
-    # tag = ''
-    match what:
-        case 'T': tag = f'<u>T</u><s>F</s>'
-        case 'F': tag = f'<s>T</s><u>F</u>'
-        case 't': tag = f'<u>t</u><s>f</s>'
-        case 'f': tag = f'<s>t</s><u>f</u>'
-        case _:
-            tag = f'<s>X</s>'
-            n = 1
-    return f'<span class=x>{ind}{_padd(otag, n, tag)}{line}</span>'
+    def _replOk(self, mo):
+        """indication: covered"""    
+        ind, what, line = mo.groups('')
+        n = 2
+        match what:
+            case 'X': 
+                tag = f'<u>&gt;</u>'
+                n = 1
+            case _:
+                tag = f'<u>{what}</u>'
+        return f'<span>{ind}{self._padd(what, n, tag)}{line}</span>'
 
-def _replOk(mo):
-    """indication: covered"""    
-    ind, what, line = mo.groups('')
-    n = 2
-    match what:
-        case 'X': 
-            tag = f'<u>&gt;</u>'
-            n = 1
-        case _:
-            tag = f'<u>{what}</u>'
-    # return f'<span>{_padd(beg, ind, tag)}{line}</span>'
-    return f'<span>{ind}{_padd(what, n, tag)}{line}</span>'
+    def _idstr(self, f:str):
+        return self.rx_id.sub('_', f) if f else '_'
+        # print(f)
+        # return self.rx_id.sub('_', f)
+    
+    def _h3(self, f:str):
+        id = ''
+        f = f[:-1]
+        if self.opts.get('i'):
+            self.curr = f
+            id = f' id="{self._idstr(self.curr)}"'
+        return f'<h3{id}>{f}</h3>'
 
-def covbr2html(fp:str, cont:str):
-    a = list(batched(rxSplit.split(cont)[1:], n=2))
-    rx_ok = re.compile(r'^( *)(X|TF|tf)( .*)?', re.M)
-    rx_nok = re.compile(r'^( *)(-->(\w+)?)( .*)?', re.M)
-    res = []
-    for f, c in a:
-        c = rx_nok.sub(_replNok, c)
-        c = rx_ok.sub(_replOk, c)
-        res.append(f'<h3>{f}</h3>\n<p>\n{c}\n</p>')
-    nfp = re.sub(r'\.\w+$', '', fp)
-    ttl = basename(nfp)
-    nfp = nfp + '.html' 
-    ncont = '\n'.join([getHtmlHeader(ttl), *res, getHtmlFooter()])
-    cont = ''
-    if isfile(nfp):
-        with open(nfp, 'r') as fh:
-            cont = fh.read()
-            fh.close()
-    if ncont != cont:
-        print('->', nfp)
-        with open(nfp, 'w') as fh:
-            fh.write(ncont)
-            fh.close()
+    def _ilink(self, f:str, n:int, w:int):
+        return f'<a href="#{self._idstr(f)}">{str(n).rjust(w)}: {f}</a>'
+
+    def _index(self):
+        minc = int(self.opts.get('i', 0))
+        if (not minc) or len(self.counter) < minc: return ''
+        print('-> index')
+        a = sorted(self.counter.items(), key=lambda x:(-x[1],x[0]))
+        w = len(str(a[0][1]))
+        return '\n'.join([self._ilink(f, n, w) for f, n in a if f])
+
+    def _2html(self, fp:str, cont:str):
+        self.counter.clear()
+        a = list(batched(self.rxSplit.split(cont)[1:], n=2))
+        res = []
+        for f, c in a:
+            c = escape(c)
+            c = self.rx_nok.sub(self._replNok, c)
+            c = self.rx_ok.sub(self._replOk, c)
+            res.append(f'{self._h3(f)}\n<p>\n{c}\n</p>')
+        nfp = re.sub(r'\.\w+$', '', fp)
+        ttl = basename(nfp)
+        nfp = nfp + '.html' 
+        ncont = '\n'.join([self._header(ttl), self._index(), *res, self._footer()])
+        cont = ''
+        if isfile(nfp):
+            with open(nfp, 'r') as fh:
+                cont = fh.read()
+                fh.close()
+        if ncont != cont:
+            print('->', nfp)
+            with open(nfp, 'w') as fh:
+                fh.write(ncont)
+                fh.close()        
 
 if __name__ == '__main__':
-    reduceCovbrCLI(func=covbr2html, doc=__doc__)
+    CovbrHtml().processCLI()
