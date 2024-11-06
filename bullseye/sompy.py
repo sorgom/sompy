5d389c3fba8ad492850efcc0590575fb2e1eec44
sorgom <110917257+sorgom@users.noreply.github.com>
2024-11-06 16:31:55 +0100

    cleanTxt newline fix

--- bullseye/sompy.py
+++
@@ -1,6 +0,0 @@
-# This file is used to add sompy/somutil to the sys.path
-from sys import path as syspath
-from os.path import dirname, abspath, join
-sompath = abspath(join(dirname(__file__), '../somutil'))
-if sompath not in syspath:
-    syspath.insert(0, sompath)
