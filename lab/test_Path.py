94b8e0225199c2f78a784799a3005efb007d7935
sorgom <110917257+sorgom@users.noreply.github.com>
2026-02-01 21:50:13 +0100

    Path takes about 4 times longer than scandir

--- lab/test_Path.py
+++ lab/test_Path.py
@@ -1,52 +1,73 @@
+from collections.abc import MutableMapping
 from pathlib import Path
 from os.path import dirname, realpath, abspath, join
 
-p = Path(dirname(dirname(__file__)))
-print(p)
-print(p.name)
-print(len(str(p / '1')))
-print(str(p / '1')[20:])
+root = dirname(dirname(__file__))
 
-class FF_Path(Path):
-    def __init__(self, offset:int, p, data:tuple=()):
-        super().__init__(p)
-        print('self', str(self))
-        self.relpath = lambda : str(self)[offset:]
+class FF_Entry():
+    def __init__(self, offset:int, p:Path, data:tuple=()):
+        self.path = str(p)
+        self.name = p.name
+        self.relpath = self.path[offset:]
+        self.mtime = p.stat().st_mtime
         self.data = data
-    def iterdir(self):
-        Path(self).iterdir()
 
-
-fp = FF_Path(12, dirname(__file__))
-
-print(fp.__fspath__())
-print(fp.relpath())
-
-fp2 = Path(fp)
+fp = Path(__file__)
+rp = Path(root)
 
 class Maker():
     def __init__(self, root):
         self.__root = Path(root)
         print('root', self.__root)
-        offset = len(str(self.__root / 'a')) - 1
-        print('offset', offset)
-        def _mkgen(offset):
-            return lambda *args : FF_Path(offset, *args)
-        self.gen = _mkgen(offset)
-        # self.gen = lambda *args : FF_Path(offset, *args)
+        offset = len(str(self.__root / 'X')) - 1
+        self.__gen = lambda p, *d : FF_Entry(offset, p, *d)
+
+        self.rescan()
+        print('data', len(self.__data))
+        print(self.__data[0])
+
+    def rescan(self):
+        self.__data = tuple(x for x in self.__recurse(self.__root))
+
+    def __iter__(self):
+        for de in self.__data: yield de
+
+    def __recurse(self, p:Path):
+        ep = self.__gen(p)
+        # print('entry:', ep.relpath, ep.mtime)
+        # apply exclude criteria on ep
+        cs = tuple(c for c in p.iterdir())
+        # print('children:', len(cs))
+        # print('fs', len(fs))
+        # print('ds', len(ds))
+        # check if dir entry valid (take criteria entry and data)
+        ep.data = tuple(self.__gen(c) for c in cs if c.is_file())
+        yield ep
+        for dp in cs:
+            if dp.is_dir():
+                for x in self.__recurse(dp): yield x
 
 
-m = Maker(p)
-fp = m.gen(__file__)
-print(type(fp).__name__)
-print('relpath', fp.relpath())
-print('name', fp.name)
-print('data', fp.data)
-if fp.data: print('data!')
+m = Maker(root)
+for de in m:
+    print('de', de.relpath, len(de.data))
 
-dp = m.gen(dirname(__file__))
-print(type(dp).__name__)
-print('relpath', dp.relpath())
-# print('dict', *dp.__dir__(), sep="\n")
-for x in dp.iterdir():
-    print(x)
+# fe = m.gen(fp)
+# print(fe, fe.relpath, fe.name, fe.mtime)
+
+# fp = m.gen(rp)
+# print(fp, fp.relpath, fp.name, fp.mtime)
+
+# fp = m.gen(__file__)
+# print(type(fp).__name__)
+# print('relpath', fp.relpath())
+# print('name', fp.name)
+# print('data', fp.data)
+# if fp.data: print('data!')
+
+# dp = m.gen(dirname(__file__))
+# print(type(dp).__name__)
+# print('relpath', dp.relpath())
+# # print('dict', *dp.__dir__(), sep="\n")
+# for x in dp.iterdir():
+#     print(x)
--- somutil/ff.py
+++ somutil/ff.py
@@ -8,7 +8,7 @@ from dirTools import *
 
 class FF_Entry:
     """wrapper for os.DirEntry"""
-    def __init__(self, offset:int, entry:DirEntry, data=None):
+    def __init__(self, offset:int, entry:DirEntry, data=tuple()):
         self.relpath = lambda : self.entry.path[offset:]
         self.stat    = lambda : self.entry.stat()
         self.name    = lambda : self.entry.name
@@ -17,9 +17,6 @@ class FF_Entry:
         self.entry   = entry
         self.data    = data
 
-    # def __mkRel(self, offset):
-    #     return lambda : self.entry.path[offset:]
-
 
 class FF_Base:
     """find files base class"""
@@ -27,19 +24,17 @@ class FF_Base:
     def __init__(self, root:str, listEmpty=False):
         self.__root     = realpath(abspath(root))
         chkDir(self.__root)
-        self._isF       = lambda e: e.is_file(follow_symlinks=False)
-        self._isD       = lambda e: e.is_dir (follow_symlinks=False)
 
-        self._checksTF  = [self._isF]
+        self._checksTF  = []
         self._checksXF  = []
         self._checksTD  = []
         self._checksXD  = []
 
-        self._checkF    = lambda *p : all(f(*p) for f in self._checksTF) and not any(f(*p) for f in self._checksXF)
-        self._checkTD   = lambda *p : all(f(*p) for f in self._checksTD)
-        self._checkXD   = lambda *p : not any(f(*p) for f in self._checksXD)
+        self._checkF    = lambda fe : all(c(fe) for c in self._checksTF) and not any(c(fe) for c in self._checksXF)
+        self._checkTD   = lambda de : all(c(de) for c in self._checksTD)
+        self._checkXD   = lambda de : not any(c(de) for c in self._checksXD)
 
-        if not listEmpty: self.addCheckTD(lambda e, fs, *p: fs)
+        if not listEmpty: self.addCheckTD(lambda de: de.data)
 
         offset = len(join(self.__root, ''))
 
@@ -47,7 +42,7 @@ class FF_Base:
 
         self.__caseSense = None
 
-        self.reScan()
+        self.__data = None
 
     def addCheckTF(self, func):
         self._checksTF.append(func)
@@ -62,12 +57,18 @@ class FF_Base:
         self._checksXD.append(func)
 
     def errcnt(self):
+        self.__chkData()
         return self.__errcnt
 
     def dircnt(self):
+        self.__chkData()
         return self.__dircnt
 
+    def path(self, e:FF_Entry):
+        return join(self.__root, e.relpath())
+
     def genMap(self, keyFuncF=None, keyFuncD=None):
+        self.__chkData()
         return self._FF_Map_D(self.__data, keyFuncF, keyFuncD)
 
     def __mkGen(self, offset:int):
@@ -77,29 +78,42 @@ class FF_Base:
         self.__errcnt = 0
         self.__dircnt = 0
 
-    def __recDirs(self, de):
+    def __recDirs(self, d:DirEntry):
         self.__dircnt += 1
         #   stop recursion if exclusion matched
+        de = self._gen(d)
         if self._checkXD(de):
             try:
-                with scandir(de.path) as iter:
+                # print('scan', d.path)
+                with scandir(d.path) as iter:
+                    # print('OK ...')
                     es = tuple(iter)
-                    fs = tuple(e for e in (self._gen(e) for e in es if self._checkF(e)))
-                    ds = tuple(e for e in es if self._isD(e))
+                    # fs = tuple(e for e in es if e.is_file())
+                    de.data = tuple(fe for fe in (self._gen(f) for f in (e for e in es if e.is_file())) if self._checkF(fe))
+                    # print('data', len(de.data))
+                    # ds = tuple(e for e in es if self._isD(e))
                     #   yield data if indicated
-                    if self._checkTD(de, fs):
-                        yield self._gen(de, fs)
-                    for d in ds:
-                        for x in self.__recDirs(d): yield x
-            except Exception:
+                    if self._checkTD(de):
+                        # print('yield')
+                        yield de
+                    for e in es:
+                        # print(e.name, e.is_dir())
+                        if e.is_dir():
+                            for x in self.__recDirs(e): yield x
+            except Exception as e:
+                # print(e)
                 self.__errcnt += 1
                 pass
 
-    def reScan(self):
+    def update(self):
         self.__reset()
         self.__data = tuple(de for de in self.__recDirs(self.__StartEntry(self.__root)))
 
+    def __chkData(self):
+        if self.__data is None: self.update()
+
     def __iter__(self):
+        self.__chkData()
         for de in self.__data: yield de
 
     def isCaseSense(self):
@@ -182,7 +196,7 @@ class FF_Re(FF_Base):
     def __genRX(pat:str, ignoreCase:bool):
         opts = [re.I] if ignoreCase else []
         rx = re.compile(pat, *opts)
-        return lambda e, *p : rx.match(e.name)
+        return lambda e : rx.match(e.name())
 
 from xglob import XGlob
 class FF_XGlob(FF_Re):
@@ -194,3 +208,25 @@ class FF_XGlob(FF_Re):
         xg = XGlob()
         tr = lambda x : xg.glob2rp(x)
         super().__init__(root, tr(xgTF), tr(xgXF), tr(xgTD), tr(xgXD), listEmpty, ignoreCase)
+
+if __name__ == '__main__':
+    from sys import argv
+    if len(argv) < 1: exit()
+    from stopWatch import StopWatch
+
+    sw = StopWatch()
+    args = (arg if arg else None for arg in argv[1:])
+    ffx = FF_XGlob(*args)
+    ffx.update()
+    sw.stop().sec()
+    print(ffx.dircnt())
+    # for e in ffx:
+    #     print(e.name())
+
+    rx = re.compile(r'^(.*)\..+$')
+    m = ffx.genMap(lambda c : rx.sub(r'\1', c))
+    for de in ffx:
+        mf = m.get(de)
+        for fe in de.data:
+            r = mf.get(fe)
+            print(r.name())
